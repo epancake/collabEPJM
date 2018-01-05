f91fa7488be8ab5ca38cb4c71d488908ec452092
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-01-05 12:10:19 -0700

    created app.js and added multiply array function

---
+++ app/app.js
@@ -0,0 +1,10 @@
+var nums = [1, 2, 3, 4, 5];
+
+function multiply(array) {
+  var result = 1;
+  for (var i = 0; i < array.length; i++) {
+    result = result * array[i];
+  }
+  return result;
+}
+console.log(multiply(nums));
